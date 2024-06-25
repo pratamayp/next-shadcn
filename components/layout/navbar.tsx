98f68104520baa32f92ffa469c7baa79e351c08e
Yoga Pratama <yogapr.dev@gmail.com>
2024-06-25 17:03:37 +0700

    feat(layout): mobile sidebar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,7 +1,14 @@
+import { cn } from '@/lib/utils';
+import { MobileSidebar } from './mobile-sidebar';
+
 export const Navbar = () => {
   return (
     <div className="fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
-      <nav className="flex h-16 items-center justify-between px-4">Navbar</nav>
+      <nav className="flex h-16 items-center justify-between px-4">
+        <div className={cn('block md:!hidden')}>
+          <MobileSidebar />
+        </div>
+      </nav>
     </div>
   );
 };
